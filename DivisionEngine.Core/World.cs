28c7033de26762ec812946b5daf074d3eea851e1
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2025-11-24 15:59:30 -0500

    First implementation of reflection system api

--- DivisionEngine.Core/World.cs
+++ DivisionEngine.Core/World.cs
@@ -1,20 +1,31 @@
-﻿namespace DivisionEngine
+﻿using System;
+using System.Reflection;
+
+namespace DivisionEngine
 {
     public class World
     {
         public HashSet<uint> entities;
         public Dictionary<Type, Dictionary<uint, IComponent>> components;
-        public List<ISystem> systems;
+        public List<SystemBase> systems;
 
+        private List<SystemBase> awakeSystems, updateSystems, fixedUpdateSystems, renderSystems;
         private uint nextEntityId;
 
         public World()
         {
-            entities = new HashSet<uint>();
-            components = new Dictionary<Type, Dictionary<uint, IComponent>>();
-            systems = new List<ISystem>();
+            entities = [];
+            components = [];
+            systems = [];
+
+            awakeSystems = [];
+            updateSystems = [];
+            fixedUpdateSystems = [];
+            renderSystems = [];
             nextEntityId = 0;
         }
+
+        #region entities
 
         public uint CreateEntity()
         {
@@ -36,6 +47,74 @@
             }
             return false;
         }
+
+        #endregion
+        #region systems
+
+        public void RegisterAllSystems()
+        {
+            foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type t in a.GetTypes())
+                {
+                    if (typeof(SystemBase).IsAssignableFrom(t) && !t.IsAbstract)
+                    {
+                        if (Activator.CreateInstance(t) is SystemBase sys)
+                        {
+                            RegisterSystem(sys);
+                        }
+                        else throw new NotImplementedException($"System of type {t} is not implemented correctly!");
+                    }
+                }
+            }
+        }
+
+        private void RegisterSystem(SystemBase system)
+        {
+            systems.Add(system);
+
+            Type sysBaseType = typeof(SystemBase), sysType = system.GetType();
+            MethodInfo? awakeInfo = sysType.GetMethod("Awake"),
+                updateInfo = sysType.GetMethod("Update"),
+                fixedUpdateInfo = sysType.GetMethod("FixedUpdate"),
+                renderInfo = sysType.GetMethod("Render");
+
+            if (awakeInfo != null && awakeInfo.DeclaringType != sysBaseType)
+                awakeSystems.Add(system);
+            if (updateInfo != null && updateInfo.DeclaringType != sysBaseType)
+                updateSystems.Add(system);
+            if (fixedUpdateInfo != null && fixedUpdateInfo.DeclaringType != sysBaseType)
+                fixedUpdateSystems.Add(system);
+            if (renderInfo != null && renderInfo.DeclaringType != sysBaseType)
+                renderSystems.Add(system);
+        }
+
+        public void CallAwake()
+        {
+            for (int i = 0; i < awakeSystems.Count; i++)
+                awakeSystems[i].Awake();
+        }
+
+        public void CallUpdate()
+        {
+            for (int i = 0; i < updateSystems.Count; i++)
+                updateSystems[i].Update();
+        }
+
+        public void CallFixedUpdate()
+        {
+            for (int i = 0; i < fixedUpdateSystems.Count; i++)
+                fixedUpdateSystems[i].FixedUpdate();
+        }
+
+        public void CallRender()
+        {
+            for (int i = 0; i < renderSystems.Count; i++)
+                renderSystems[i].Render();
+        }
+
+        #endregion
+        #region components
 
         public bool AddComponent<T>(uint entityId, T component) where T : IComponent
         {
@@ -83,6 +162,9 @@
             return components.ContainsKey(type) && components[type].ContainsKey(entityId);
         }
 
+        #endregion
+        #region queries
+
         public IEnumerable<uint> Query<T>() where T : IComponent
         {
             Type type = typeof(T);
@@ -90,5 +172,7 @@
                 return components[type].Keys;
             return [];
         }
+
+        #endregion
     }
 }
