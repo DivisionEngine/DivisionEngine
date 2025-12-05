b264fdb5087e2e84c0e264113b9276c6a7c23e94
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2025-12-05 00:54:36 -0500

    Scaffold some parts of assets window

--- DivisionEngine/Views/AssetsWindow.axaml.cs
+++ DivisionEngine/Views/AssetsWindow.axaml.cs
@@ -2,8 +2,14 @@ using Avalonia.Controls;
 
 namespace DivisionEngine.Editor;
 
-public partial class AssetsWindow : UserControl
+public partial class AssetsWindow : EditorWindow
 {
+    private readonly ScrollViewer scrollViewer;
+    private readonly WrapPanel assetsPanel;
+
+    private readonly StackPanel header;
+    private readonly TextBlock headerText;
+
     public AssetsWindow()
     {
         InitializeComponent();
