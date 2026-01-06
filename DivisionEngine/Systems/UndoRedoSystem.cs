2c7b7f2ae05ae639993da475b82e3c1ff7a4a989
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2026-01-05 21:49:16 -0500

    Scaffold undo redo system

---
+++ DivisionEngine/Systems/UndoRedoSystem.cs
@@ -0,0 +1,10 @@
+﻿namespace DivisionEngine.Editor.Systems
+{
+    /// <summary>
+    /// Manages undo and redo functionality for the Division editor.
+    /// </summary>
+    public class UndoRedoSystem : SystemBase
+    {
+
+    }
+}
