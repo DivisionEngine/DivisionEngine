811df8f65adccd1c2336a5953010ef726e2843c8
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2026-01-26 22:01:35 -0500

    Begin range attribute implementation

---
+++ DivisionEngine.Core/Components/FieldAttributes/RangeAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace DivisionEngine.Components.FieldAttributes
+{
+
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public class RangeAttribute : Attribute
+    {
+
+        public float Minimum { get; set; } = 0f;
+        public float Maximum { get; set; } = 1f;
+
+        public RangeAttribute() { }
+    }
+}
