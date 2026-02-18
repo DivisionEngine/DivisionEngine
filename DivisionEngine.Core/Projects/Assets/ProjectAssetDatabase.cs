83ccd65d834b6ad55aa5a14d14d353effd3ab166
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2026-02-17 19:44:11 -0500

    Scaffold asset database system

---
+++ DivisionEngine.Core/Projects/Assets/ProjectAssetDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisionEngine.Projects.Assets
+{
+    internal class ProjectAssetDatabase
+    {
+    }
+}
