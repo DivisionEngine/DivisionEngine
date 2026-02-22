db511372569b8286236155f557b89b397fa09270
Rex Woodfield <42045447+Rex-J-W@users.noreply.github.com>
2026-02-22 01:35:24 -0500

    Add license headers

--- DivisionEngine.Core/Components/Lights/DirectionalLight.cs
+++ DivisionEngine.Core/Components/Lights/DirectionalLight.cs
@@ -1,4 +1,22 @@
-﻿using DivisionEngine.MathLib;
+﻿//
+// Copyright (C) 2026 Rex Woodfield
+//
+// This file is part of Division Engine.
+//
+// Division Engine is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Division Engine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Division Engine.  If not, see <https://www.gnu.org/licenses/>.
+//
+using DivisionEngine.MathLib;
 
 namespace DivisionEngine.Components.Lights
 {
